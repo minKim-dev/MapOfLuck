209ee4a513ad6715702d7a7806d1baa884032694
minKim-dev <lovejd0815@gmail.com>
2024-02-25 14:18:32 +0900

    TextField for storing rate is changed into Slider and some variables is added.

--- MapOfLuck/ContentView.swift
+++ MapOfLuck/ContentView.swift
@@ -26,66 +26,85 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var locationName: String = ""
-    @State var rate: String = "" // rate arrangement is 0 to 5
+    @State private var locationName: String = ""
+    @State private var locationNames: [String] = ["Example1", "Example2"] // for testing
+    @State private var rate: Double = 0.0 // rate arrangement is 0 to 5
+    @State private var rates: [Double] = [3.0, 4.0] // for testing
     @State private var isEditing = false
-    var info: [String : Int] = [:] // locationName : rate
-    @State private var errorNumber: Int
-    @State var color: String = "black"
+    var info: [String : String] = [:] // locationName : rate
+    // @State private var errorNumber: Int
+    @State private var color: Color  = .black
+    
     
     
     var body: some View {
+         
         // NavigationStack 사용해보기
         NavigationStack {
-            TextField(
-                "locationName",
-                text: $locationName
-            )
-            .foregroundColor(.color)
             
-           // rate를 표시할 수 있는 TextField 추가함.
-            TextField(
-                "0 to 5",
-                text: $rate
-            )
-        
-            NavigationLink(destination: ListView()) {
-                Text("complete")
+            Text("MAP of Luck")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 100)
+            
+            VStack {
+                TextField(
+                    "locationName",
+                    text: $locationName
+                )
+                .foregroundColor(color)
+                .padding()
+               // rate를 표시할 수 있는 TextField 추가함.
+                Text("Slider Value: \(Int(rate))")
+                
+                Slider(value: $rate, in: 0...5, step: 1)
+                    .padding()
+                    .accentColor(.blue)
+            
+                NavigationLink(destination: ListView(rate: $rate, locationNames: $locationNames)) {
+                    Text("complete")
+                }
+                
+                // the place for errorDetect() func
+                // 텍스트 필드 색깔을 받아서 변경하는 것으로
             }
-            
-            // the place for errorDetect() func
-            // 텍스트 필드 색깔을 받아서 변경하는 것으로
+            .padding(.bottom, 100)
         }
     }
     
+    /*
     // Error handling
     private func errorDetect() {
         // first case: locationName and rate are empty.
         // Second case: the type typed at rate TextField is wrong; not number type because String is stored on rate TextField.
         
-        // if statement -> return value according to case
+        // if statement -> return value according to error case number.
         if locationName == "\0" && rate == "\0" {
             errorNumber = 1
         }
         // switch statement -> error handling based on return value.
         
     }
+     */
 }
     
 struct ListView: View {
+    @Binding var rate: Double
+    @Binding var locationNames: [String]
+    
     var body: some View {
             Text("This is ListView")
         
         NavigationStack {
-            // ForEach 이용해서 List 표현하는 방식으로 바꿨음.
-            ForEach(0..<10, id: \.self) { num in
-                List() {
-                    NavigationLink(destination: DetailView()) {
-                        Text("location_\(num)")
-                    }
+            // ForEach 이용해서 List 표현하는 방식으로 바꿨음. -> 이러면 뷰가 2개 생겨서 상단과 하단에 똑같은 뷰가 생겨버림.
+            // ForEach를 없애고 배열을 사용해서 리스트를 구현함.
+            List(locationNames, id: \.self) { location in
+                NavigationLink(destination: DetailView()) {
+                    Text("location_\(location)")
                 }
-                
             }
+                
+            
         }
     }
 }
